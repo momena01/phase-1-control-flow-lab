806fb213ca9cbc9df0841589333d2239c284dff5
momena <momena.salloum59@gmail.com>
2023-09-27 17:56:14 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+    // Write your code here!
+ 
+    if (ride <= 400) {
+        return 'This one is on me!'  
+    }
+    else if (ride >= 400 && ride <= 2000) {
+        return 'That will be twenty bucks.'
+    }
+    else if (ride >= 2000 && ride <= 2499 ) { 
+        return'I will gladly take your thirty bucks.'
+    }
+    else if (ride >= 2500) {
+        return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    // Write your code here!
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(trip){
+    // Write your code here!
+    switch (trip) {
+        case 'generous':
+            return "Thank you so much."
+        case 'not as generous':
+            return "Thank you."
+        default:
+            return 'Bye.'
+    }
 }
+
+
+
